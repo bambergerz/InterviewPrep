4da832ba788d2d44e92211759e704c0d27524817
Zachary Bamberger <zeb3@cornell.edu>
2017-11-18 19:01:57 -0500

    Implemented naive version of deijkra's shortest path algorithm (i.e., did not use a priority queue for it).

--- graph.py
+++ graph.py
@@ -1,5 +1,5 @@
-from Queue import Queue
-from Queue import PriorityQueue
+from Queue import Queue, PriorityQueue
+import heapq
 import logging
 import os
 import sys
@@ -17,6 +17,7 @@ class Node(object):
         self._id = Node.node_ID
         self._value = value
         self._neighbors = set()
+        self._edges = {}
         Node.node_ID += 1
 
     def __repr__(self):
@@ -30,6 +31,17 @@ class Node(object):
         assert isinstance(other, Node)
         return self.value < other.value
 
+    def __eq__(self, other):
+        print(type(self))
+        print(type(other))
+        assert isinstance(other, Node)
+        return self._value == other.value
+
+    def __copy__(self):
+        cp = Node(self.value)
+        for n in self.neighbors:
+            cp.add_neighbor(n)
+        return cp
 
     @property
     def node_id(self):
@@ -43,14 +55,37 @@ class Node(object):
     def neighbors(self):
         return self._neighbors
 
-    def add_neighbor(self, neighbor):
+    @property
+    def edges(self):
+        return self._edges
+
+    def set_value(self, value):
+        self._value = value
+
+    def add_neighbor(self, neighbor, weight=0):
         """
         
         :param neighbor: a neighbor node of this Node instance
+        :param weight: the weight of the edge from the current node to its neighbor
         :return: True once "neighbor" has been appended to this node's list of neighbors. 
         """
         assert isinstance(neighbor, Node)
         self._neighbors.add(neighbor)
+        edge = Edge(self, neighbor, weight)
+        self._edges[neighbor] = edge
+
+    def get_edge(self, target):
+        """
+        
+        :param target: return the edge between the current node and the target node if it exists. 
+        If this edge does not exist, return None.
+        :return: 
+        """
+        if target in self.neighbors:
+            return self.edges[target]
+        else:
+            print "This node does not share an edge with node: " + str(target)
+            return None
 
 
 class Edge(object):
@@ -98,22 +133,25 @@ class Graph(object):
         self._edges = {}  # map a source node to all the edges which leave from it.
         self._bidirectional = bidirectional
         self._id = Graph.graph_ID
-
-        # set up the logger
-        # filename = "graph%d.log" % self._id
-        # cur_dir = os.getcwd()
-        # if filename in os.listdir(cur_dir):
-            # os.remove(filename)
-        logging.basicConfig(filename="graph%d.log" % self._id,
-                            level=logging.INFO,
-                            format='%(asctime)s:\n%(message)s\n\n',
-                            datefmt='%m/%d/%Y %I:%M:%S %p')
+        self.set_up_logger()
 
     def __str__(self):
         msg = ""
         msg += "Nodes are: " + str(self._nodes) + "\n"
         msg += "Edges are: " + str(self._edges.values())
         return msg
+
+    def set_up_logger(self):
+        # set up the logger
+        filename = "graph%d.log" % self._id
+        cur_dir = os.getcwd()
+        if filename in os.listdir(cur_dir):
+            os.remove(filename)
+        logging.basicConfig(filename="graph%d.log" % self._id,
+                            level=logging.INFO,
+                            format='%(asctime)s:\n%(message)s\n\n',
+                            datefmt='%m/%d/%Y %I:%M:%S %p')
+
 
     @property
     def nodes(self):
@@ -224,7 +262,7 @@ class Graph(object):
 
         while True:
             if frontier.empty():
-                msg += "failed"
+                msg += "failed\n"
                 logging.info(msg)
                 if to_print:
                     print msg
@@ -235,7 +273,7 @@ class Graph(object):
             children = node.neighbors
             for child in children:
                 if child.node_id == target_node:
-                    msg += str(child)
+                    msg += str(child) + "\n"
                     logging.info(msg)
                     if to_print:
                         print msg
@@ -271,7 +309,7 @@ class Graph(object):
 
         while not found:
             if len(frontier) == 0:
-                msg += "failed"
+                msg += "failed\n"
                 logging.info(msg)
                 if to_print:
                     print msg
@@ -282,7 +320,7 @@ class Graph(object):
             children = node.neighbors
             for child in children:
                 if child.node_id == target_node:
-                    msg += str(child)
+                    msg += str(child) + "\n"
                     logging.info(msg)
                     if to_print:
                         print msg
@@ -344,22 +382,51 @@ class Graph(object):
         
         :param source: the source of the propagation Deisjkstra's algorithm.
         an int which represents a valid node id.
-        :param to_print: True if you wish to print the profression of the algorithn. False otherwise. 
+        :param to_print: True if you wish to print the progression of the algorithm. False otherwise. 
         :return: a tuple consisting of (distances, predecessors)
         distances is an array of nodes
         predecessors is an array of nodes.
         """
-        # TODO: Implement a priority queue before trying this
-        origin = self.get_node_by_id(source)
-        predecessors = {}
         distances = {}
-        visited = set()
-        queue = PriorityQueue()
-        for node in self.nodes:
-            predecessors[node.node_id] = None
-            distances[node.node_id] = sys.maxint
-            queue.put(node)
-        distances[source] = 0
+        predecessors = {}
+        Q = []
+        source_node = self.get_node_by_id(source)
+        for x in self.nodes:
+            distances[x] = sys.maxint
+            predecessors[x] = None
+            Q.append(x)
+        distances[source_node] = 0
+
+        if to_print:
+            print("Initial distances are " + str(distances))
+            print("Initial predecessors are " + str(predecessors) + "\n")
+
+        while len(Q) > 0:
+            min_dist = sys.maxint
+            min_node = None
+            for node, dist in distances.items():
+                if dist < min_dist:
+                    min_node = node
+                    min_dist = dist
+
+            Q.remove(min_node)
+
+            for v in min_node.neighbors:
+                assert isinstance(v, Node)
+                alternative = distances[min_node] + min_node.get_edge(v).weight
+                if alternative < distances[v]:
+                    distances[v] = alternative
+                    predecessors[v] = min_node
+
+            if to_print:
+                print("Updated distances are: " + str(distances))
+                print("Updated predecessors are: " + str(predecessors) + "\n")
+
+        if to_print:
+            print("Final distances are: " + str(distances))
+            print("Updated predecessors are: " + str(predecessors))
+
+        return distances, predecessors
 
 
 if __name__ == "__main__":
@@ -378,7 +445,7 @@ if __name__ == "__main__":
     g.add_edge(ab)
     g.add_edge(bc)
     g.add_edge(cd)
-    g.bfs(0, 4, to_print=True)
-    g.dfs(0, 4, to_print=True)
+    g.bfs(0, 3, to_print=True)
+    g.dfs(0, 3, to_print=True)
     distances, predecessors = g.bellmen_ford(0, to_print=True)
-    g.deijkstra(0, to_print=True)
+    distances, predecessors = g.deijkstra(0, to_print=True)
